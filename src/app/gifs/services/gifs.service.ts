b8e1f8c909d6716338579924634a2e6a6c598684
EmanuelTejada032 <emanueltejada31295@gmail.com>
2021-07-14 10:48:48 -0400

    Add http module from angular

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -12,14 +13,20 @@ export class GifsService {
     return [...this._history];
   }
 
+  constructor( private http: HttpClient) { }
+
   addGifs(query: string): void{
     query = query.trim().toLocaleLowerCase();
       if(!this._history.includes(query)){
           this._history.unshift(query);
           this._history = this._history.splice(0, 10);
       }
-      
+
+      //http module from angular return an observable, offers more functinality than fetch
+      this.http.get('https://api.giphy.com/v1/gifs/search?api_key=yourapikey&q=cheeseburgers&limit=10&lang=en')
+      .subscribe( (res: any) => {
+          console.log(res.data);
+      })
   }
 
-  constructor() { }
 }
