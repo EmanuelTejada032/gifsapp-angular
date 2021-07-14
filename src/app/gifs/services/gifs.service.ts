8120a8f4208f85ed3fc20de73f77867c6ac0f634
EmanuelTejada032 <emanueltejada31295@gmail.com>
2021-07-14 12:02:01 -0400

    Store history data on local store

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -15,17 +15,25 @@ export class GifsService {
     return [...this._history];
   }
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient) {
+    const localData = localStorage.getItem("history")
+    if(localData){
+      this._history = JSON.parse(localData!)
+    }
+      
+  }
 
   addGifs(query: string): void{
     query = query.trim().toLocaleLowerCase();
       if(!this._history.includes(query)){
           this._history.unshift(query);
           this._history = this._history.splice(0, 10);
+          localStorage.setItem("history",JSON.stringify(this._history));
+          
       }
 
       //http module from angular return an observable, offers more functinality than fetch
-      this.http.get<GifsSearchResponse>(`https://api.giphy.com/v1/gifs/search?api_key=yourapikey&q=${query}&limit=10&lang=en`) //Return a gifsSearchResponse interface
+      this.http.get<GifsSearchResponse>(`https://api.giphy.com/v1/gifs/search?api_key=sRYMDEom54Yx0YpoZn5TnuAm7XlvUnXJ&q=${query}&limit=10&lang=en`) //Return a gifsSearchResponse interface
       .subscribe( (res) => {
           console.log(res.data);
           this.gifsResults = res.data;
